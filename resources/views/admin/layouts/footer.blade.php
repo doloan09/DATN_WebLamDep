63806ef7e34375ef3d8677ce787a84efa85a9d27
Do Thi Loan <dothiloan150901@gmail.com>
2023-02-13 17:39:49 +0700

    config cloudinary

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="text-center user-select-none">
+    <p class="small m-n">
+        © Copyright {{date('Y')}}
+        <a href="{{ config('hosts.web') }}" target="_blank">AMARA.STORE</a><br>
+        {{ __('Version') }}: {{config('platform.version')}}
+    </p>
+</div>
---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,15 @@
+@push('head')
+    <link
+        href="https://img.ws.mms.shopee.vn/9c7774ea5157eeb888580960dfeb825c_tn"
+        id="favicon"
+        rel="icon"
+    >
+@endpush
+
+<div class="h2 fw-light d-flex align-items-center">
+    <img src="https://img.ws.mms.shopee.vn/9c7774ea5157eeb888580960dfeb825c_tn" class="w-3x rounded-pill" alt="logo">
+    <p class="ms-3 my-0 d-none d-sm-block">
+        AMARA
+        <small class="align-top opacity">Store</small>
+    </p>
+</div>
