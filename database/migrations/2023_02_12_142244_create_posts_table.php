cd46b60ae23e4c8ea97691f9088efba7a1a580c7
Do Thi Loan <dothiloan150901@gmail.com>
2023-02-13 17:35:36 +0700

    create migration

---
+++ database/migrations/2023_02_12_142244_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('title');
+            $table->longText('content');
+            $table->unsignedInteger('id_category');
+            $table->timestamps();
+            $table->foreign('id_category')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2023_02_12_142305_create_wishlists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedInteger('id_post');
+            $table->timestamps();
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_post')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlists');
+    }
+};
