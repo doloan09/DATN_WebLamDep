036e29cd4c69d124a02de668ff11489ca7698418
Do Thi Loan <dothiloan150901@gmail.com>
2023-06-11 23:32:10 +0700

    fix

--- resources/views/client/posts/list.blade.php
+++ resources/views/client/posts/list.blade.php
@@ -7,6 +7,14 @@
         <div class="py-4">
             <div class="flex flex-wrap border-l-4 border-purple">
                 <a href="{{ route('home') }}" class="px-3 hover:text-purple">Trang chủ</a>
+                @if($category->child_id)
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-5">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </span>
+                    <p class="px-3">Tham khảo</p>
+                @endif
                 <span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-5">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
@@ -75,7 +83,7 @@
 
 @push('scripts')
     <script>
-        check();
+        // check();
         function check(){
             if ('{{ session('wishlist_err') }}'){
                 alert('{{ session('wishlist_err') }}');
