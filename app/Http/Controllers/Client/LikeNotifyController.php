1f756206c8bf9bca50b57980e6afc9659e321112
Do Thi Loan <dothiloan150901@gmail.com>
2023-06-17 09:57:02 +0700

    fix like notification

--- app/Http/Controllers/Client/LikeNotifyController.php
+++ app/Http/Controllers/Client/LikeNotifyController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Notification;
+use App\Models\Post;
 
 class LikeNotifyController extends Controller
 {
     public function store($id, $slug) // đánh dấu thông báo là đã đọc, đồng thời chuyên sang trang bài viết tương ứng
     {
         Notification::where('id', $id)->update(['read_at' => now()]);
-        return redirect()->route('posts.show', $slug);
+        $post     = Post::query()->where('slug', $slug)->first();
+        $category = Category::query()->findOrFail($post->id_category);
+        return redirect()->route('posts.show', ['category' => $category->slug, 'slug' => $slug]);
     }
 }
